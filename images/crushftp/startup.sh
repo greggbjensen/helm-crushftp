19ecb57af355003f713703aa285d21f80e81f2a6
Gregg B. Jensen <greggbjensen@gmail.com>
2021-11-01 14:59:36 -0600

    Updated symbolic links to be created on each startup, since they would be lost otherwise, and moved admin_user_set.txt.

--- images/crushftp/startup.sh
+++ images/crushftp/startup.sh
@@ -4,32 +4,34 @@ FTP_CONFIG_BASE_DIR="/mnt/config"
 
 # Create persisted volume directories and symbolically link to correct CrushFTP path.
 if [[ ! -f ${FTP_CONFIG_BASE_DIR}/config_initialized.txt ]] ; then
-    echo "Creating symbolic links for FTP config..."
+    echo "Creating initial directories FTP config..."
 
     if [[ ! -f ${FTP_CONFIG_BASE_DIR} ]] ; then
         mkdir -p ${FTP_CONFIG_BASE_DIR}
     fi
 
-    cd ${FTP_CONFIG_BASE_DIR} && mkdir backup logs settings SavedReports users syncsDB statsDB
-    ln -s ${FTP_CONFIG_BASE_DIR}/backup ${CRUSH_FTP_BASE_DIR}/backup
-    ln -s ${FTP_CONFIG_BASE_DIR}/logs ${CRUSH_FTP_BASE_DIR}/logs
-    ln -s ${FTP_CONFIG_BASE_DIR}/settings ${CRUSH_FTP_BASE_DIR}/settings
-    ln -s ${FTP_CONFIG_BASE_DIR}/SavedReports ${CRUSH_FTP_BASE_DIR}/SavedReports
-    ln -s ${FTP_CONFIG_BASE_DIR}/users ${CRUSH_FTP_BASE_DIR}/users
-    ln -s ${FTP_CONFIG_BASE_DIR}/syncsDB ${CRUSH_FTP_BASE_DIR}/syncsDB
-    ln -s ${FTP_CONFIG_BASE_DIR}/statsDB ${CRUSH_FTP_BASE_DIR}/statsDB
+    # Do not symbolically link logs or it will impact performance.
+    cd ${FTP_CONFIG_BASE_DIR} && mkdir backup settings SavedReports users syncsDB statsDB
     touch ${FTP_CONFIG_BASE_DIR}/config_initialized.txt
 
     echo "Because this was an intial setup, you will want to restart the pod once fully ready for best performance."
 fi
 
+echo "Establishing symbolic links..."
+ln -s ${FTP_CONFIG_BASE_DIR}/backup ${CRUSH_FTP_BASE_DIR}/backup
+ln -s ${FTP_CONFIG_BASE_DIR}/settings ${CRUSH_FTP_BASE_DIR}/settings
+ln -s ${FTP_CONFIG_BASE_DIR}/SavedReports ${CRUSH_FTP_BASE_DIR}/SavedReports
+ln -s ${FTP_CONFIG_BASE_DIR}/users ${CRUSH_FTP_BASE_DIR}/users
+ln -s ${FTP_CONFIG_BASE_DIR}/syncsDB ${CRUSH_FTP_BASE_DIR}/syncsDB
+ln -s ${FTP_CONFIG_BASE_DIR}/statsDB ${CRUSH_FTP_BASE_DIR}/statsDB
+
 if [ -z ${CRUSH_ADMIN_USER} ]; then
     CRUSH_ADMIN_USER=crushadmin
 fi
 
-if [ -z ${CRUSH_ADMIN_PASSWORD} ] && [ -f ${CRUSH_FTP_BASE_DIR}/admin_user_set.txt ]; then
+if [ -z ${CRUSH_ADMIN_PASSWORD} ] && [ -f ${FTP_CONFIG_BASE_DIR}/admin_user_set.txt ]; then
     CRUSH_ADMIN_PASSWORD="NOT DISPLAYED!"
-elif [ -z ${CRUSH_ADMIN_PASSWORD} ] && [ ! -f ${CRUSH_FTP_BASE_DIR}/admin_user_set.txt ]; then
+elif [ -z ${CRUSH_ADMIN_PASSWORD} ] && [ ! -f ${FTP_CONFIG_BASE_DIR}/admin_user_set.txt ]; then
     CRUSH_ADMIN_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
 fi
 
@@ -41,10 +43,10 @@ if [ -z ${CRUSH_ADMIN_PORT} ]; then
     CRUSH_ADMIN_PORT=8080
 fi
 
-if [[ ! -f ${CRUSH_FTP_BASE_DIR}/admin_user_set.txt ]] ; then
+if [[ ! -f ${FTP_CONFIG_BASE_DIR}/admin_user_set.txt ]] ; then
     echo "Creating default admin..."
     cd ${CRUSH_FTP_BASE_DIR} && java -jar ${CRUSH_FTP_BASE_DIR}/CrushFTP.jar -a "${CRUSH_ADMIN_USER}" "${CRUSH_ADMIN_PASSWORD}"
-    touch ${CRUSH_FTP_BASE_DIR}/admin_user_set.txt
+    touch ${FTP_CONFIG_BASE_DIR}/admin_user_set.txt
 fi
 
 ${CRUSH_FTP_BASE_DIR}/crushftp_init.sh start
